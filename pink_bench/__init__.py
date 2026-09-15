c480f532e1b529ea6fedca2af300b6d8b3a3f8f7
Stéphane Caron <stephane.caron@normalesup.org>
2026-09-15 19:54:43 +0200

    minor: Clean up source file headers

--- pink_bench/__init__.py
+++ pink_bench/__init__.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
 # SPDX-License-Identifier: Apache-2.0
 
 """Library of robot motions with inverse kinematics and predictive control."""
--- pink_bench/library.py
+++ pink_bench/library.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
 # SPDX-License-Identifier: Apache-2.0
 
 """Benchmark scenarios: a robot, an initial pose and a task trajectory.
